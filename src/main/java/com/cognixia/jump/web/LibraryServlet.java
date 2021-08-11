61a9661cb7045238241af29c1755ed4ed3684b0a
Pthomas2 <pthomas1@lions.molloy.edu>
2021-08-11 10:59:10 -0400

    Updated maven and Library Servlet
    Added serial ID and updated maven project

--- src/main/java/com/cognixia/jump/web/LibraryServlet.java
+++ src/main/java/com/cognixia/jump/web/LibraryServlet.java
@@ -2,4 +2,9 @@ package com.cognixia.jump.web;
 import javax.servlet.http.HttpServlet;
 
 public class LibraryServlet extends HttpServlet {
+
+
+	private static final long serialVersionUID = 1L;
+	
+	
 }
